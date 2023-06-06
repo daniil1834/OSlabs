8c6eaa13ce6d0c5ce00ac8ed73b25d9b4e2cfeb5
daniil <kovalcukdaniil18@gmail.com>
2023-06-06 23:14:52 +0300

    lab1

---
+++ FileTreePrinter.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+//lab1
+public class FileTreePrinter {
+
+    public static void main(String[] args) {
+        File currentDir = new File(".");
+        printFileTree(currentDir, 0);
+    }
+
+    private static void printFileTree(File file, int depth) {
+        System.out.println("\t".repeat(Math.max(0, depth)) + file.getName());
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File child : files) {
+                    printFileTree(child, depth + 1);
+                }
+            }
+        }
+    }
+}
+
